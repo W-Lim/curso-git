dcb50e0e834c62c30291e9c7aa648f5b9b4d1411
Wender Lima <wenderlima678@gmail.com>
2021-07-11 21:56:43 -0300

    Acrescentada a pergunta, como voce esta hoje?

--- Proteste/Program.cs
+++ Proteste/Program.cs
@@ -5,6 +5,7 @@ namespace Proteste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como voce esta hoje??");
         }
     }
 }
