3a2f9973f310ff4e928f9e7ec132abfb9e2ff295
Wender Lima <wenderlima678@gmail.com>
2021-07-11 22:29:01 -0300

    Acrescentada a menssagem desejando um bom dia

--- Proteste/Program.cs
+++ Proteste/Program.cs
@@ -4,8 +4,10 @@ namespace Proteste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+            
             Console.WriteLine("Como voce esta hoje??");
+
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
